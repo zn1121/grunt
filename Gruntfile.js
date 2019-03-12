d68944d9297301c99d6bbe0013c7618e92f96e4d
wangding <408542507@qq.com>
2019-03-12 21:10:08 +0800

    entropy.js

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+grunt.initConfig({
+    mochacli: {
+    options: {
+            require: ['should'],
+      reporter: 'nyan',
+      bail: true
+      
+    },
+    all: ['test/*.js']
+                   
+  }
+
+})
+grunt.loadNpmTasks('grunt-mocha-cli')
+grunt.registerTask('default', ['mochacli'])
+}
