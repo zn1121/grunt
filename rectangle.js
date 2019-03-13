c2da9d76c8fa061d2e04e255af55a67ab6b7e1f1
wangding <408542507@qq.com>
2019-03-13 14:26:47 +0800

    cssmin

---
+++ rectangle.js
@@ -0,0 +1,29 @@
+$(function(){
+    var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  console.log('asdasdasd');
+
+  $btnCal.click(function(){
+        var w = $width.val();
+            var h = $height.val();
+
+                var p = 2 * ( w + h );
+                    var a = w * h;
+                        var r = new Rectangle(w,h);
+
+                            $perimeter.val(p);
+                                $area.val(a);
+                                    $perimeter.val(r.perimeter());
+                                            $area.val(r.area());
+
+                                                console.log(w);
+                                                    console.log(h);
+                                                        console.log(p);
+                                                            console.log(a);
+                                                              
+  });
+
+});
